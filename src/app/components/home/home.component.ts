fa400b3ce2bdbec7ba5c40abc74d4363c42594fa
Mizus <jankowski.michal96@gmail.com>
2023-01-18 18:28:21 +0100

    feat: added store by categorya and stores sections

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,13 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Observable, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CategoryModel } from '../../models/category.model';
+import { StoreQueryModel } from '../../query-models/store.query-model';
+import { CategoriesService } from '../../services/categories.service';
+import { StoresService } from '../../services/stores.service';
+import { TagsService } from '../../services/tags.service';
+import { StoreModel } from '../../models/store.model';
+import { TagModel } from '../../models/tag.model';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +17,27 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
+  readonly categories$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
+  readonly stores$: Observable<StoreQueryModel[]> = combineLatest([
+    this._storesService.getAllStores(),
+    this._tagsService.getAllTags()
+  ]).pipe(map(([stores, tags]) => this._mapToStoreQueryModel(stores, tags)))
+
+  constructor(private _categoriesService: CategoriesService, private _storesService: StoresService, private _tagsService: TagsService) {
+  }
+
+  private _mapToStoreQueryModel(
+    stores: StoreModel[],
+    tags: TagModel[]
+  ): StoreQueryModel[] {
+    const tagMap = tags.reduce((a, c) => ({ ...a, [c.id]: c }), {}) as Record<string, TagModel>
+
+    return stores.map((store) => ({
+      name: store.name,
+      logoUrl: store.logoUrl,
+      distanceInMeters: store.distanceInMeters,
+      tags: (store.tagIds ?? []).map((id) => tagMap[id]?.name),
+      id: store.id
+    }))
+  }
 }
