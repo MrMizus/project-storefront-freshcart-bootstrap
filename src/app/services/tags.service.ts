470d0dd528fd7307ca0e394bea39898a832e195b
Mizus <jankowski.michal96@gmail.com>
2023-01-18 18:25:58 +0100

    feat: created tag model and service for tags

---
+++ src/app/services/tags.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TagModel } from '../models/tag.model';
+
+@Injectable({ providedIn: 'root' })
+export class TagsService {
+  constructor(private _httpClient: HttpClient) {
+  }
+
+  getAllTags(): Observable<TagModel[]> {
+    return this._httpClient.get<TagModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-store-tags');
+  }
+}
