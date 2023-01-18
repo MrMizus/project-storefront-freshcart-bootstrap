24331fde5c5879b2c979508c5e09609d950f46e4
MrMizus <jankowski.michal96@gmail.com>
2023-01-18 12:02:53 +0100

    feat: added route to app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { StoreProductsComponent } from './components/store-products/store-produc
 import { CategoryProductsComponentModule } from './components/category-products/category-products.component-module';
 import { HomeComponentModule } from './components/home/home.component-module';
 import { StoreProductsComponentModule } from './components/store-products/store-products.component-module';
+import { HeaderComponentModule } from './components/header/header.component-module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponentModule } from './components/footer/footer.component-module';
+import { FooterComponent } from './components/footer/footer.component';
 
 const routes: Routes = [
   { path: 'categories/:categoryId', component: CategoryProductsComponent },
@@ -19,7 +23,9 @@ const routes: Routes = [
     CategoryProductsComponentModule,
     HomeComponentModule,
     StoreProductsComponentModule,
+    HeaderComponentModule,
+    FooterComponentModule
   ],
-  exports: [RouterModule],
+  exports: [RouterModule, HeaderComponent, FooterComponent],
 })
 export class AppRoutingModule {}
