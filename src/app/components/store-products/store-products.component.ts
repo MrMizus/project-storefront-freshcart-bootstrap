bdfa67ab2856f3718bfecd2d2fcdcd255b342422
Mizus <jankowski.michal96@gmail.com>
2023-01-20 20:20:51 +0100

    feat: added store page

--- src/app/components/store-products/store-products.component.ts
+++ src/app/components/store-products/store-products.component.ts
@@ -1,4 +1,12 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest, Observable, startWith } from 'rxjs';
+import { switchMap, map, shareReplay, debounceTime,} from 'rxjs/operators';
+import { StoreModel } from '../../models/store.model';
+import { ProductModel } from '../../models/product.model';
+import { StoresService } from '../../services/stores.service';
+import { ProductsService } from '../../services/products.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-store-products',
@@ -8,4 +16,23 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StoreProductsComponent {
+  readonly search: FormGroup = new FormGroup({ searchProduct: new FormControl() });
+  readonly store$: Observable<StoreModel> = this._activatedRoute.params.pipe(switchMap(data => this._storesService.getOneStore(data["storeId"])),
+   shareReplay(1));
+
+   readonly startsWith$: Observable<string> = this.search.valueChanges.pipe(
+    map(form => form.searchProduct.toLocaleLowerCase()),
+    startWith(''),
+    debounceTime(1000)
+  );
+
+
+  readonly products$: Observable<ProductModel[]> = combineLatest([
+    this.store$,
+    this._productsService.getAllProducts(),
+    this.startsWith$
+  ]).pipe(map(([store, produts, startsWith]) => produts.filter((product) => (product.storeIds.includes(store.id) && product.name.toLocaleLowerCase().includes(startsWith)))))
+
+  constructor(private _storesService: StoresService, private _activatedRoute: ActivatedRoute, private _productsService: ProductsService) {
+  }
 }
